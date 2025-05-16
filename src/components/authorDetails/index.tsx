817b85fa0ae44e858a9aa6b2501604568ebb1031
Guilherme Oliveira <sirguilhermeoliveira@gmail.com>
2025-05-16 00:41:33 -0300

    refactor: updated author details component

--- src/components/authorDetails/index.tsx
+++ src/components/authorDetails/index.tsx
@@ -1,15 +1,17 @@
 import { User } from "@/types/user";
 
 
-export default function AuthorDetails({ email, phone, website, address, company }: User) {
+export default function AuthorDetails({ email, phone, website, address, company, title, name, username }: User) {
   return (
-    <section aria-label="Author details" className="mb-12 p-6 bg-blue-50 rounded-lg shadow-md">
+    <section aria-label="Author details" className="mb-12 p-4 pb-1 bg-blue-50 rounded-lg shadow-md">
       <h2 className="text-2xl font-semibold text-blue-700 border-b border-blue-600 pb-2 mb-6">
-        About the Author
+       {title ?? "About the Author"}
       </h2>
-
+    <div className="flex items-start">
       <section aria-label="Contact info" className="mb-6">
         <h3 className="text-lg font-semibold text-blue-800 mb-2">Contact Information</h3>
+        {name && <p><strong>Name:</strong> {name}</p>}
+        {username && <p><strong>Username:</strong> {username}</p>}
         <p>
           <strong>Email:</strong>{' '}
           <a href={`mailto:${email}`} className="text-blue-600 hover:underline">{email}</a>
@@ -25,9 +27,10 @@ export default function AuthorDetails({ email, phone, website, address, company 
 
       <section aria-label="Address info" className="mb-6">
         <h3 className="text-lg font-semibold text-blue-800 mb-2">Address</h3>
-        <p>{address.street}, {address.suite}</p>
-        <p>{address.city} - {address.zipcode}</p>
-        <p>Coordinates: {address.geo.lat}, {address.geo.lng}</p>
+        <p><strong>Street: </strong>{address.street}, {address.suite}</p>
+        <p><strong>City: </strong>{address.city}</p>
+        <p><strong>Zip code: </strong>{address.zipcode}</p>
+        <p><strong>Coordinates: </strong> {address.geo.lat}, {address.geo.lng}</p>
       </section>
 
       <section aria-label="Company info">
@@ -36,6 +39,7 @@ export default function AuthorDetails({ email, phone, website, address, company 
         <p className="italic text-gray-600">"{company.catchPhrase}"</p>
         <p>{company.bs}</p>
       </section>
+      </div>
     </section>
   );
 }
